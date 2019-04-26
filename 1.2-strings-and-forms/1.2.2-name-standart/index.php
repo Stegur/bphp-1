e98f400d49faa8276e1c9fa68662b38d97bc4b6e
Stegura Alex <stegura.alex@gmail.com>
2019-04-26 23:31:57 +0300

    1.2.2

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Stegur
- * Date: 26.04.2019
- * Time: 23:02
- */
+
+//$firstName = $_POST['firstName'];
+//$lastName = $_POST['lastName'];
+//$middleName = $_POST['middleName'];
+
+$firstName = 'иван';
+$lastName = 'иванов';
+$middleName = 'иванович';
+
+$fullName = ucfirst($lastName) . ' ' . ucfirst($firstName) . ' ' . ucfirst($middleName);
+
+$fio = mb_strtoupper(mb_substr($lastName, 0, 1)) . mb_strtoupper(mb_substr($firstName, 0, 1)) . mb_strtoupper(mb_substr
+    ($middleName, 0, 1));
+
+$surnameAndInitials = ucfirst($lastName) . ' ' . mb_strtoupper(mb_substr($firstName, 0, 1)) . '.' . mb_strtoupper
+(mb_substr
+    ($middleName, 0, 1)) . '.';
+
+echo "Полное имя: '$fullName' <br>"; // Не работает
+echo "Фамилия и инициалы: '$fio'  <br>";
+echo "Аббревиатура: '$surnameAndInitials' <br>";// Часть неработает
+
