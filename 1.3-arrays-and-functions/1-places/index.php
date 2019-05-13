40d7cf6d21d2538b40f1bc9962c3f5555a92307f
Stegura Alex <stegura.alex@gmail.com>
2019-05-13 23:14:15 +0300

    1.3.1

--- 1.3-arrays-and-functions/1-places/index.php
+++ 1.3-arrays-and-functions/1-places/index.php
@@ -1,7 +1,53 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Stegur
- * Date: 27.04.2019
- * Time: 22:39
- */
+
+
+function generate($rows, $placesPerRow, $avaliableCount) {
+
+    if ($rows * $placesPerRow > $avaliableCount) {
+        return false;
+    }
+
+    $map = [];
+
+    for ($i = 0; $i < $rows; $i++) {
+        for ($j = 0; $j < $placesPerRow; $j++) {
+            $map[$i][$j] = false;
+        }
+    }
+
+    return $map;
+}
+
+function reserve(&$map, $row, $place) {
+
+    if ($map[$row+1][$place+1] === false) {
+        $map[$row+1][$place+1] = true;
+        return true;
+    }
+}
+
+
+//  то функци должна - опечатка
+
+
+
+
+$chairs = 50;
+$map = generate(5, 8, $chairs);
+$requireRow = 3;
+$requirePlace = 5;
+
+$reverve = reserve($map, $requireRow, $requirePlace);
+logReserve($requireRow, $requirePlace, $reverve);
+
+$reverve = reserve($map, $requireRow, $requirePlace);
+logReserve($requireRow, $requirePlace, $reverve);
+
+
+function logReserve($row, $place, $result){
+    if ($result) {
+        echo "Ваше место забронировано! Ряд $row, место $place".PHP_EOL;
+    } else {
+        echo "Что-то пошло не так=( Бронь не удалась".PHP_EOL;
+    }
+}
