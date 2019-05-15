3035f9ffd0e15554756044bd55ce4658e1126c93
Stegura Alex <stegura.alex@gmail.com>
2019-05-15 23:23:33 +0300

    fix

--- 1.2-strings-and-forms/1.2.1-registration/index.php
+++ 1.2-strings-and-forms/1.2.1-registration/index.php
@@ -10,42 +10,45 @@ $code = $_POST['code'];
 
 $codeWord = 'nd82jaake';
 
-$isCorrect = false;
+$isCorrect = true;
 
 $patternLogin = '/\W/';
 if (preg_match($patternLogin, $login)) {
     echo 'Поле логин не должно содержать символы @/*?,;:. <br>';
+    $isCorrect = false;
 }
 
 if (strlen($pass) < 8) {
     echo 'Длина пароля должна быть минимум 8 символов <br>';
+    $isCorrect = false;
 }
 
 $patternEmail = '/\A[^@]+@([^@\.]+\.)+[^@\.]+\z/';
 if (!preg_match($patternEmail, $email)) {
     echo 'Почта должна быть формата почта@домен.доменнаязона <br>';
+    $isCorrect = false;
 }
 
 if (strlen($firstName) === 0) {
     echo 'Поле Имя не может быть пустым <br>';
+    $isCorrect = false;
 }
 
 if (strlen($lastName) === 0) {
     echo 'Поле Фамилия не может быть пустым <br>';
+    $isCorrect = false;
 }
 
 if (strlen($middleName) === 0) {
     echo 'Поле Отчество не может быть пустым <br>';
+    $isCorrect = false;
 }
 
 if ($codeWord !== trim(strtolower($code))) {
     echo 'Вы неверно указали Кодовое слово <br>';
+    $isCorrect = false;
 }
 
-// не могу придумать... пытался через флаг, но тогда последняя проверка решает исход...
 if ($isCorrect) {
     echo 'Вы успешно зарегистрировались!<br>';
 }
-
-// Ошибка в Создать файлы form.html и index.php
-// Ошибка в Поля Фамилия, Имя, Отчество не могут быть пустыми
