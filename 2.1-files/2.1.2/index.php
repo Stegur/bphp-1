fd78d71b802ce4a6f7e4aed89aed5f9aa0dc8195
Stegura Alex <stegura.alex@gmail.com>
2019-05-19 23:30:49 +0300

    task 2

---
+++ 2.1-files/2.1.2/index.php
@@ -0,0 +1,41 @@
+<?php
+
+$uploadDir = 'images/';
+$uploadFile = $uploadDir . basename($_FILES['image']['name']);
+
+
+if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile)) {
+    echo 'Картинка загружена';
+}
+
+$files = scandir($uploadDir);
+
+?>
+
+
+<!doctype html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<title>Gallery</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<form action="index.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="image">
+
+		<input type="submit" name="Загрузить">
+	</form>
+    <?php
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..') {
+//            if (exif_imagetype($file) != IMAGETYPE_GIF) {
+                echo "<img class=\"image\" src=\"" . $uploadDir . $file . "\" >";
+//            }
+        }
+
+    }
+    ?>
+	<img src="" alt="">
+</body>
+</html>
